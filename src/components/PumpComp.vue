8f961f2dba12e080911d7e3ad00c5eecf5e7c082
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-07-24 15:43:55 +0200

    Update PumpComp.vue

--- src/components/PumpComp.vue
+++ src/components/PumpComp.vue
@@ -1,5 +1,17 @@
 <script setup>
+import { ref } from 'vue'
 import { fuels } from '../assets/store.js'
+const selected = ref({
+    id: 0
+})
+
+
+const actualQuantity = ref(0)
+const quantityBought = ref(0)
+
+function deliver(){
+
+}
 
 </script>
 
@@ -10,15 +22,30 @@ import { fuels } from '../assets/store.js'
         </select>
         <div>
             <label for="limit">Limite</label>
-            <input type="number" id="limit">
+            <input v-model.number="quantityBought" type="number" id="limit">
             <input type="radio" id="liter" name="limits" checked>
             <label for="liter">litres</label>
             <input type="radio" id="money" name="limits">
             <label for="money">€</label>
         </div>
-        <div>
+        <div class="purchase-elements">
             <label for="price-per-liter">Prix du litre</label>
-            <input id="price-shown" type="text" readonly>
+            <input id="price-per-liter" type="text" readonly :value="fuels[selected.id].pricePerLiter">
+            <p>€</p>
+        </div>
+        <div class="purchase-elements">
+            <label for="quantity">Quantité</label>
+            <input v-model.number="actualQuantity" id="quantity" type="text" readonly>
+            <p>litres</p>
+        </div>
+        <div class="purchase-elements">
+            <label for="price-to-pay">À payer</label>
+            <input id="price-to-pay" type="text" readonly>
+            <p>€</p>
+        </div>
+        <div class="trigger">
+            <p class="gauge"><span class="filled-gauge" :style="{width: `${actualQuantity / quantityBought *100}%`}"> </span></p>
+            <button type="button" @click="deliver">Lancer la distribution</button>
         </div>
     </form>
 </template>
@@ -31,12 +58,12 @@ form{
     color: var(--vt-c-gold);
 }
 
-select, option, input{
-    width: 75%;
+select, option, input, button{
+    width: 65%;
     height: 25px;
     color: var(--vt-c-white-soft);
     background-color: var(--vt-c-red2);
-    border-color: var(--vt-c-red);
+    border: 1px solid var(--vt-c-red);
 }
 
 div{
@@ -44,10 +71,19 @@ div{
     border-bottom: 2px solid var(--vt-c-white-soft);
 }
 
+label{
+    width: 25%;
+}
+
 #limit{
     border: 1px solid var(--vt-c-red);
     width: 15%;
     margin: 0 20px;
+}
+
+#price-shown{
+    width: 10%;
+    margin-right: 5px;
 }
 
 input[type='radio'] {
@@ -62,5 +98,30 @@ input[type='radio'] {
 
 input[type='radio']:checked {
     background: var(--vt-c-gold);
+}
+
+.purchase-elements{
+    display: flex;
+}
+
+.gauge{
+    width: 100%;
+    height: 25px;
+    background-color: var(--vt-c-red2);
+    border: 1px solid var(--vt-c-red);
+}
+
+.filled-gauge{
+    display: inline-block;
+    height: 21px;
+    width: 0%;
+    margin-top: 1px;
+    padding: 1px 0;
+    background-color: var(--vt-c-gold);
+}
+
+button{
+    width: 100%;
+    margin-top: 10px;
 }
 </style>
